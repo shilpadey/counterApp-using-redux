f65619fdab14d704c5ab5f426b36b99d9e6cf1b3
shilpadey <shilpadey735@gmail.com>
2022-12-25 16:53:13 +0530

    incremented and decremented by 2

---
+++ src/components/Counter.js
@@ -0,0 +1,33 @@
+
+import { useDispatch, useSelector } from 'react-redux';
+import classes from './Counter.module.css';
+
+const Counter = () => {
+  const dispatch = useDispatch();
+
+  const counter = useSelector(state => state.counter)
+
+  const incrementHandler = () => {
+    dispatch({ type : 'incrementby2' });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type : 'decrementby2' });
+  };
+
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
